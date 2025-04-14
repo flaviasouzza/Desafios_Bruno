7a2285f4a52b029328fb001c7c8274156a312e2a
Flávia Souza <88092488+flaviasouzza@users.noreply.github.com>
2025-04-14 12:01:32 -0300

    Update index.php

--- 1-ListadeTarefas/index.php
+++ 1-ListadeTarefas/index.php
@@ -1,1 +1,42 @@
+<?php
+session_start();																	
+
+if(isset($_SESSION['tarefas'])){													
+if(isset($_POST['tarefa'])){															
+	array_push($_SESSION['tarefas'], $_POST['tarefa']);				
+}
+} else {																				
+	$_SESSION['tarefas'] = [];														
+	if(isset($_POST['tarefa'])){														
+	array_push($_SESSION['tarefas'], $_POST['tarefa']);		
+}
+}
+
+?>
+
+<!DOCTYPE html>																			
+<html lang= "pt-br">																	
+<head>
+    <title>TaskFlow</title>															
+</head>
+
+<center>  																				
+<h1 style = "color: green"> Bem Vindo ao TaskFlow </h1> 								
+</center> 
+  
+<body>
+<form method="post">																	
+  
+    <label for="tarefa"> Tarefas:</label> 												
+    <input id="tarefa" name="tarefa"/>													
+    <button type="submit">Salvar</button> 												
+</form>
+<?php
+	$tarefas = $_SESSION['tarefas'];													
+	foreach($tarefas as $tarefa){														
+		echo $tarefa . "<br>";													
+	}
+?>
+</body>
+</html>
 
